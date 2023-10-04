1a647341fa10f7e20f4d400454e6739d98bf7f19
nate-daba <ndaba@arizona.edu>
2023-10-03 19:17:10 -0700

    blink LED

---
+++ iot/blink-LED/src/blink-LED.ino
@@ -0,0 +1,24 @@
+/*
+ * Project blink-LED
+ * Description: blink an LED on and off
+ * Author: Natnael Daba
+ * Date: October 3, 2023
+ */
+int light = D7; // This is where your LED is plugged in. The other side goes to a resistor connected to GND.
+// setup() runs once, when the device is first turned on.
+SYSTEM_THREAD(ENABLED);
+
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  pinMode(light, OUTPUT);
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  digitalWrite(light, HIGH);   // Turn ON the LED pins
+  delay(1000);                       // Wait for 1000mS = 1 second
+  digitalWrite(light, LOW);    // Turn OFF the LED pins
+  delay(1000);                       // Wait for 1 second in off mode
+}
---
+++ iot/blink-LED/target/4.2.0/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+/Users/natnaeldaba/Documents/Documents/Academia/UofA/Third_Semester/ECE_513_Web_dev_and_IoT/final_project/heart-rate-monitoring-system-design/iot/blink-LED/target/4.2.0/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
