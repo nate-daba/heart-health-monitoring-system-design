022556413822f92c994d2811670ef311331c0fb4
nate-daba <ndaba@arizona.edu>
2023-11-27 15:37:04 -0700

    Input verification code, needs to append error messages.

--- Nates_server/public/javascripts/signup.js
+++ Nates_server/public/javascripts/signup.js
@@ -2,27 +2,89 @@
 function signUp(e) {
     e.preventDefault();
     $('.errorDiv').hide();
-    // data validation
-    if ($('#email').val() === "") {
-        window.alert("Email can not be empty.");
-        return;
-    }
-    if ($('#password').val() === "") {
-        window.alert("Password can not be empty.");
-        return;
-    }
-    // data validation
-    if ($('#firstName').val() === "") {
-        window.alert("First name can not be empty.");
-        return;
-    }
-    if ($('#lastName').val() === "") {
-        window.alert("Last name can not be empty.");
+
+    let email = $(`#email`);
+    let password = $(`#password`);
+    let firstName = $(`#firstName`);
+    let lastName = $(`#lastName`);
+
+    // email is not empty
+    if (email.val() === "") {
+        $('.errorDiv p').text("Email cannot be empty.");
+        $('.errorDiv').show();
+        //window.alert("Email can not be empty.");
         return;
     }
 
-    if($('#password').val() !== $('#confirmPassword').val()) {
-        window.alert("Passwords do not match.");
+    // new - email is valid
+    let emailReq = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    if ((!(email.match(emailReq)))) {
+        //window.alert("Invalid email address.");
+        $('.errorDiv p').text("Invalid email address.");
+        $('.errorDiv').show();
+        return;
+    }
+
+    // password is not empty
+    if (password.val() === "") {
+        window.alert("Password can not be empty.");
+        $('.errorDiv p').text("Password can not be empty.");
+        $('.errorDiv').show();
+        return;
+    }
+
+    // new - password is between 10 and 20 characters
+    if (!((password.length < 10) || (password.length > 20))) {             
+        window.alert("Password must be between 10 and 20 characters in length.");
+        $('.errorDiv p').text("Password must be between 10 and 20 characters in length.");
+        $('.errorDiv').show();
+    }
+
+    // new - password contains a lowercase character
+    let lowCase = /[a-z]/;
+    if (!(password.match(lowCase))) {
+        window.alert("Password must contain at least one lowercase character.");
+        $('.errorDiv p').text("Password must contain at least one lowercase character.");
+        $('.errorDiv').show();
+    }
+
+    // new - password contains an uppercase character
+    let upCase = /A-Z/;
+    if (!(password.match(upCase))) {
+        window.alert("Password must contain at least one uppercase character.");
+        $('.errorDiv p').text("Password must contain at least one uppercase character.");
+        $('.errorDiv').show();
+    }
+
+    // new - password contains a digit
+    let digitCheck = /[0-9]/;
+    if ((!(password.match(digitCheck)))) {
+        window.alert("Password must contain at least one digit.");
+        $('.errorDiv p').text("Password must contain at least one digit.");
+        $('.errorDiv').show();
+    }
+
+    // passwrod and confirmation password match
+    if(password.val() !== $('#confirmPassword').val()) {
+        window.alert("Password and confirmation password do not match.");
+        $('.errorDiv p').text("Passwrd and confirmation password do not match.");
+        $('.errorDiv').show();
+        return;
+    }
+
+    // first name field is not empty
+    if (firstName.val() === "") {
+        window.alert("First name can not be empty.");
+        $('.errorDiv p').text("First name can not be empty.");
+        $('.errorDiv').show();
+        return;
+    }
+
+    // last name field is not empty
+    if (lastName.val() === "") {
+        window.alert("Last name can not be empty.");
+        $('.errorDiv p').text("Last name can not be empty.");
+        $('.errorDiv').show();
         return;
     }
 
