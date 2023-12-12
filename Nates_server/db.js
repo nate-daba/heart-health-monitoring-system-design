ffc50fae41183bcb391669e5f7aee0f85d5be4ec
nate-daba <ndaba@arizona.edu>
2023-12-11 21:23:33 -0700

    final signup and login changes

--- Nates_server/db.js
+++
@@ -1,5 +0,0 @@
-// to use MongoDB
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://MeTheOnly:GuteArbeit891234@ece513project.ldg8mzo.mongodb.net/hearto2monitor?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology:true });
-
-module.exports = mongoose;
